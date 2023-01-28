882730d9baf4273dc55977f8f181dbc07dff06fc
Lora <lorat1403@gmail.com>
2023-01-28 18:34:06 +0200

    add folder helpers

---
+++ src/helers/index.js
@@ -0,0 +1,9 @@
+const httpError = require("./httpError");
+const handleMongooseError = require("./handleMongooseError");
+const ctrlWrapper = require("./ctrlWrapper");
+
+module.exports = {
+  httpError,
+  handleMongooseError,
+  ctrlWrapper,
+};
