c210a68130add860975f26f4a6b88ee123c1d594
Pabearyoga <pabearyoga@gmail.com>
2023-02-01 14:28:19 +0200

    fix addNotice petsAvatarURL if !req.file

--- src/controllers/notices.js
+++ src/controllers/notices.js
@@ -47,20 +47,38 @@ const getById = async (req, res, next) => {
 const avatarsDir = path.join(__dirname, '../', 'public', 'avatars');
 
 const addNotice = async (req, res, next) => {
-  const { path: tempUpload, originalname } = req.file;
   const { _id: id } = req.user;
+
+  if (!req.file) {
+    const imageName = 'petly.png';
+
+    try {
+      const resultUpload = path.join(avatarsDir, imageName);
+      await fs.rename(resultUpload, resultUpload);
+      const petsAvatarURL = path.join('public', 'avatars', imageName);
+      const result = await Notice.create({
+        ...req.body,
+        petsAvatarURL,
+        owner: id,
+      });
+      res.json(result);
+    } catch (error) {
+      next(error);
+    }
+    return;
+  }
+
+  const { path: tempUpload, originalname } = req.file;
   const imageName = `${id}_${originalname}`;
 
   try {
-    // const petsAvatarURL = "https://images.app.goo.gl/tZR2WarKP6wYJKA87";
     const resultUpload = path.join(avatarsDir, imageName);
     await fs.rename(tempUpload, resultUpload);
     const petsAvatarURL = path.join('public', 'avatars', imageName);
-    const { _id } = req.user;
     const result = await Notice.create({
       ...req.body,
       petsAvatarURL,
-      owner: _id,
+      owner: id,
     });
     res.status(201).json(result);
   } catch (error) {
