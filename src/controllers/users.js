700a2955815cd7d4945a41fca64ce104ed60203d
Pabearyoga <pabearyoga@gmail.com>
2023-02-03 12:36:16 +0200

    fix deleteAvatarsController

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -291,12 +291,15 @@ const deleteAvatarsController = async (req, res, next) => {
   try {
     const { _id } = req.user;
 
-    const result = await User.findByIdAndUpdate(_id, { avatarURl: null });
+    const result = await User.findByIdAndUpdate(
+      { _id: _id },
+      { avatarURL: null }
+    );
 
-    if (result === null) {
+    if (!result) {
       throw HttpError(404, 'Not found');
     }
-    res.json(result);
+    res.json({ avatarURL: null });
   } catch (error) {
     next(error);
   }
