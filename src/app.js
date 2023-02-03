dcd7bad91ddc59ce543e2c501ed25fba829d1491
Oksana Bulakh <ksushavin15@gmail.com>
2023-02-03 22:45:36 +0100

    avatars, add notice

--- src/app.js
+++ src/app.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger.json');
 
 const app = express();
+
+
+
+
+
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+
+
+
+
+
 
 app.use((req, res) => {
   res.status(404).json({ code: "not-found", message: "Path wasn't found" });
@@ -20,5 +29,11 @@ app.use((req, res) => {
 app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
+
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app;
